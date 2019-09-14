c5e20d13c74f6ff738e71c8ddccea2aad2cb3751
Tianna Mosby <mosbyts@vcu.edu>
2019-09-14 10:07:27 -0400

    Added basic HTML.

---
+++ Assets/JavaScript/JavaScript.js
@@ -0,0 +1,8 @@
+var topics = [Pumpkins, Bats, Clowns, Witches, candyCorn];
+var button = "<button>";
+//a loop that appends a button for each string in the array.
+
+for (var i = 0; i > topics.length; i++);
+    i.append(button);
+
+button.onclick();
